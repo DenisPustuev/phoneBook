2c5073cefb580ec4397f6bafb281099338b67107
Denis Pustuev <pustuev.denis@gmail.com>
2015-06-14 14:23:24 +0600

    implemented localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,114 @@
-var phoneBookApp = angular.module('phoneBookApp', []);
+var phoneBookApp = angular.module('phoneBookApp', ['LocalStorageModule']);
 
-phoneBookApp.controller('phoneBookCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter){
+phoneBookApp.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+        .setPrefix('phoneBookApp')
+        .setStorageCookie(0, '/');
+});
 
-    $http.get('users.json').success(function (data) {
-        $scope.contacts = $filter('orderBy')(data, 'name');
-        $scope.currentContact = $scope.contacts[0];
-    });
+phoneBookApp.controller('phoneBookCtrl', ['$scope', '$http', '$filter', 'localStorageService', function($scope, $http, $filter, localStorageService){
+
+    if(localStorageService.isSupported){
+
+        if(localStorageService.length()){
+            //localStorageService.clearAll();
+            var allContacts = localStorageService.get('allContacts');
+
+            getContacts(allContacts);
+
+        }else{
+            $http.get('users.json').success(function (data) {
+
+                getContacts(data);
+
+            });
+        }
+
+    }
+
+
 
     $scope.showAnchor = function ($index){
 
-        var thisAnchor = $scope.contacts[$index].name[0];
+        var thisAnchor = $scope.allContacts[$index].name[0];
         var prevAnchor;
 
         if($index >0){
-            prevAnchor = $scope.contacts[$index - 1].name[0];
+            prevAnchor = $scope.allContacts[$index - 1].name[0];
         }
 
         return (thisAnchor !== prevAnchor);
 
     };
 
-    $scope.showContactDetails = function (contact) {
+    $scope.showFavAnchor = function ($index) {
+        if($index == 0) return true;
+    };
+
+    $scope.showContactDetails = function (contacts, contact) {
+
+        for(var i = 0; i < contacts.length; i++){
+            if(contacts[i].active == true) {
+                contacts[i].active = false;
+            }
+        }
         $scope.currentContact = contact;
+        $scope.currentContact.active = true;
+        localStorageService.set('allContacts', $scope.allContacts);
+    };
+
+
+    $scope.favCtrlFunc = function (currentContact) {
+
+        if(currentContact.favorite){
+            $scope.favContacts.splice($scope.favContacts.indexOf(currentContact), 1);
+            $scope.favContacts = $filter('orderBy')($scope.favContacts, 'name');
+            currentContact.favorite = false;
+            localStorageService.set('allContacts', $scope.allContacts);
+
+        }else{
+            $scope.favContacts.push(currentContact);
+            $scope.favContacts = $filter('orderBy')($scope.favContacts, 'name');
+            currentContact.favorite = true;
+            localStorageService.set('allContacts', $scope.allContacts);
+        }
+
+    };
+
+    function getContacts(contactsObj) {
+        $scope.allContacts = $filter('orderBy')(contactsObj, 'name');
+
+        $scope.favContacts = [];
+
+        angular.forEach($scope.allContacts, function(obj) {
+
+            if(obj.active == true){
+                $scope.currentContact = obj;
+            }
+
+            if(obj.favorite){
+                this.push(obj);
+            }
+        }, $scope.favContacts);
+
+        if($scope.currentContact == undefined){
+            if($scope.favContacts.length > 0){
+
+                $scope.currentContact = $scope.favContacts[0];
+            }else{
+                $scope.currentContact = $scope.allContacts[0];
+            }
+
+            $scope.currentContact.active = true;
+        }
+
+        $scope.currentContactIsFav = function (currentContact) {
+            if(currentContact.favorite){
+                return ""
+            }else{
+                return "-empty"
+            }
+        }
     }
+
 }]);
